92149e7f36b5b29af72eca6ca169c6dad47e8d6b
Kevin <550031@student.saxion.nl>
2024-05-08 15:36:31 +0200

    Changed configs and added tweakables

---
+++ GameData.cs
@@ -0,0 +1,11 @@
+﻿namespace GXPEngine
+{
+    internal static class GameData
+    {
+        public static readonly Vec2 PlayerSpawnPosition = new Vec2(Game.main.width / 2, Game.main.height / 2); // The spawn position of the player
+        public static readonly float PlayerMaxVelocity = 10f; // The maximum velocity the player can have
+        public static readonly int PlayerRadius = 32; // The radius of the player
+
+        public static readonly int GlobalArrowSize = 30; // The size of the arrows
+    }
+}
--- Player.cs
+++ Player.cs
@@ -10,29 +10,37 @@ namespace GXPEngine
 
         private bool _isPlayerMoving = false;
 
-        public Player() : base(32, "circle.png", 1, 1)
+        public Player() : base(GameData.PlayerRadius, "circle.png", 1, 1)
         {
-            Collider.SetPosition(new Vec2(game.width / 2, game.height / 2));
+            Collider.SetPosition(GameData.PlayerSpawnPosition);
             Collider.LoseVelocityOverTime = true;
-            velocityArrow = new Arrow(Vec2.zero, Collider.Velocity, 30);
-            shootStrengthArrow = new Arrow(Vec2.zero, Vec2.zero, 30);
+            velocityArrow = new Arrow(Vec2.zero, Collider.Velocity, GameData.GlobalArrowSize);
+            shootStrengthArrow = new Arrow(Vec2.zero, Vec2.zero, GameData.GlobalArrowSize);
             AddChild(velocityArrow);
             AddChild(shootStrengthArrow);
         }
 
         private void Update()
         {
+            if (Input.GetKeyDown(Key.R)) ResetPlayer();
             _isPlayerMoving = !Vec2.IsZero(Collider.Velocity, 0.01f);
             CheckMousePosition();
             UpdateArrows();
+            PringDebugData();
+        }
+
+        private void ResetPlayer()
+        {
+            Collider.SetPosition(GameData.PlayerSpawnPosition);
+            Collider.SetVelocity(Vec2.zero);
         }
 
         private void CheckMousePosition()
         {
             Vec2 mouseVector = new Vec2(Input.mouseX, Input.mouseY) - Collider.Position;
-            if (!_isPlayerMoving) shootStrengthArrow.vector = mouseVector.Normalized() * Mathf.Clamp((mouseVector.Length() / 30), 0f, 10f);
+            if (!_isPlayerMoving) shootStrengthArrow.vector = mouseVector.Normalized() * Mathf.Clamp((mouseVector.Length() / 30), 0f, GameData.PlayerMaxVelocity);
             else shootStrengthArrow.vector = Vec2.zero;
-            if (Input.GetMouseButtonDown(0) && !_isPlayerMoving) Collider.SetVelocity(mouseVector.Normalized() * Mathf.Clamp((mouseVector.Length() / 30), 0f, 10f));
+            if (Input.GetMouseButtonDown(0) && !_isPlayerMoving) Collider.SetVelocity(mouseVector.Normalized() * Mathf.Clamp((mouseVector.Length() / 30), 0f, GameData.PlayerMaxVelocity));
         }
 
         private void UpdateArrows()
@@ -40,6 +48,13 @@ namespace GXPEngine
             velocityArrow.startPoint = Collider.Position;
             shootStrengthArrow.startPoint = Collider.Position;
             velocityArrow.vector = Collider.Velocity;
+        }
+
+        private void PringDebugData()
+        {
+            Console.Clear();
+            Console.WriteLine($"Player position: {Collider.Position}");
+            Console.WriteLine($"Player velocity: {Collider.Velocity}");
         }
     }
 }
